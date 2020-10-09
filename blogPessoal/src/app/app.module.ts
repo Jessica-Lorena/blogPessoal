77f2742e7597a3ae7945792f5e089ee73d6cf783
Jessica Cristina dos Santos Lorena <jessicacslorena@gmail.com>
2020-10-09 14:18:21 -0300

    Refatoração do código, var global, hashLocation

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { DeletePostagemComponent } from './delete-postagem/delete-postagem.compo
 import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { AlertasComponent } from './alertas/alertas.component';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,7 +50,10 @@ import { AlertasComponent } from './alertas/alertas.component';
     OrderModule,
     ModalModule.forRoot() //deixa eu importar esse ModalModule para qualquer componente que eu quiser
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass:HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
